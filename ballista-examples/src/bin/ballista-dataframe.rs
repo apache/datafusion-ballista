74cc4ef1ef2f833c1763a1a7d4f073a7c41b352c
Andy Grove <andygrove73@gmail.com>
2022-05-19 20:06:26 -0600

    examples use testdata from repo

--- ballista-examples/src/bin/ballista-dataframe.rs
+++ ballista-examples/src/bin/ballista-dataframe.rs
@@ -23,13 +23,11 @@ use datafusion::prelude::{col, lit, ParquetReadOptions};
 #[tokio::main]
 async fn main() -> Result<()> {
     let config = BallistaConfig::builder()
-        .set("ballista.shuffle.partitions", "4")
+        .set("ballista.shuffle.partitcargo run --example test_sql --features=ballista/standaloneions", "4")
         .build()?;
     let ctx = BallistaContext::remote("localhost", 50050, &config).await?;
 
-    let testdata = datafusion::test_util::parquet_test_data();
-
-    let filename = &format!("{}/alltypes_plain.parquet", testdata);
+    let filename = "testdata/alltypes_plain.parquet";
 
     // define the query using the DataFrame trait
     let df = ctx
