bc30a9689586b4cbc696d20cf8145de752490a9b
Brent Gardner <bgardner@squarelabs.net>
2022-07-25 12:47:54 -0600

    Refactor for sanity

--- ballista/rust/scheduler/src/flight_sql.rs
+++ ballista/rust/scheduler/src/flight_sql.rs
@@ -41,12 +41,14 @@ use arrow_flight::SchemaAsIpc;
 use ballista_core::config::BallistaConfig;
 use ballista_core::serde::protobuf;
 use ballista_core::serde::protobuf::job_status;
+use ballista_core::serde::protobuf::CompletedJob;
 use ballista_core::serde::protobuf::JobStatus;
 use ballista_core::serde::protobuf::PhysicalPlanNode;
 use datafusion::arrow;
 use datafusion::arrow::datatypes::Schema;
 use datafusion::arrow::ipc::writer::{IpcDataGenerator, IpcWriteOptions};
 use datafusion::logical_expr::LogicalPlan;
+use datafusion::prelude::SessionContext;
 use datafusion_proto::protobuf::LogicalPlanNode;
 use prost::Message;
 use tokio::time::sleep;
@@ -64,20 +66,8 @@ impl FlightSqlServiceImpl {
             _statements: Arc::new(Mutex::new(HashMap::new())),
         }
     }
-}
 
-#[tonic::async_trait]
-impl FlightSqlService for FlightSqlServiceImpl {
-    type FlightService = FlightSqlServiceImpl;
-    // get_flight_info
-    async fn get_flight_info_statement(
-        &self,
-        query: CommandStatementQuery,
-        _request: FlightDescriptor,
-    ) -> Result<Response<FlightInfo>, Status> {
-        debug!("Got query:\n{}", query.query);
-
-        // Run query
+    async fn create_ctx(&self) -> Result<Arc<SessionContext>, Status> {
         let config_builder = BallistaConfig::builder();
         let config = config_builder
             .build()
@@ -91,15 +81,117 @@ impl FlightSqlService for FlightSqlServiceImpl {
             .map_err(|e| {
                 Status::internal(format!("Failed to create SessionContext: {:?}", e))
             })?;
+        Ok(ctx)
+    }
+
+    async fn prepare_statement(
+        query: CommandStatementQuery,
+        ctx: &Arc<SessionContext>,
+    ) -> Result<LogicalPlan, Status> {
         let plan = ctx
             .sql(query.query.as_str())
             .await
             .and_then(|df| df.to_logical_plan())
             .map_err(|e| Status::internal(format!("Error building plan: {}", e)))?;
+        Ok(plan)
+    }
 
-        // enqueue job
+    async fn check_job(&self, job_id: &String) -> Result<Option<CompletedJob>, Status> {
+        let status = self
+            .server
+            .state
+            .task_manager
+            .get_job_status(job_id)
+            .await
+            .map_err(|e| {
+                let msg = format!("Error getting status for job {}: {:?}", job_id, e);
+                error!("{}", msg);
+                Status::internal(msg)
+            })?;
+        let status: JobStatus = match status {
+            Some(status) => status,
+            None => {
+                let msg = format!("Error getting status for job {}!", job_id);
+                error!("{}", msg);
+                Err(Status::internal(msg))?
+            }
+        };
+        let status: job_status::Status = match status.status {
+            Some(status) => status,
+            None => {
+                let msg = format!("Error getting status for job {}!", job_id);
+                error!("{}", msg);
+                Err(Status::internal(msg))?
+            }
+        };
+        match status {
+            job_status::Status::Queued(_) => Ok(None),
+            job_status::Status::Running(_) => Ok(None),
+            job_status::Status::Failed(e) => Err(Status::internal(format!(
+                "Error building plan: {}",
+                e.error
+            )))?,
+            job_status::Status::Completed(comp) => Ok(Some(comp)),
+        }
+    }
+
+    async fn job_to_fetch_part(
+        &self,
+        completed: CompletedJob,
+        num_rows: &mut i64,
+        num_bytes: &mut i64,
+    ) -> Result<Vec<FlightEndpoint>, Status> {
+        let mut fieps: Vec<_> = vec![];
+        for loc in completed.partition_location.iter() {
+            let fetch = if let Some(ref id) = loc.partition_id {
+                let fetch = protobuf::FetchPartition {
+                    job_id: id.job_id.clone(),
+                    stage_id: id.stage_id,
+                    partition_id: id.partition_id,
+                    path: loc.path.clone(),
+                };
+                protobuf::Action {
+                    action_type: Some(protobuf::action::ActionType::FetchPartition(
+                        fetch,
+                    )),
+                    settings: vec![],
+                }
+            } else {
+                Err(Status::internal("Error getting partition ID".to_string()))?
+            };
+            let authority = if let Some(ref md) = loc.executor_meta {
+                format!("{}:{}", md.host, md.port)
+            } else {
+                Err(Status::internal(
+                    "Invalid partition location, missing executor metadata".to_string(),
+                ))?
+            };
+            if let Some(ref stats) = loc.partition_stats {
+                *num_rows += stats.num_rows;
+                *num_bytes += stats.num_bytes;
+            } else {
+                Err(Status::internal("Error getting stats".to_string()))?
+            }
+            let loc = Location {
+                uri: format!("grpc+tcp://{}", authority),
+            };
+            let buf = fetch.encode_to_vec();
+            let ticket = Ticket { ticket: buf };
+            let fiep = FlightEndpoint {
+                ticket: Some(ticket),
+                location: vec![loc],
+            };
+            fieps.push(fiep);
+        }
+        Ok(fieps)
+    }
+
+    async fn enqueue_job(
+        &self,
+        ctx: Arc<SessionContext>,
+        plan: &LogicalPlan,
+    ) -> Result<String, Status> {
         let job_id = self.server.state.task_manager.generate_job_id();
-
         self.server
             .state
             .task_manager
@@ -111,7 +203,6 @@ impl FlightSqlService for FlightSqlServiceImpl {
 
                 Status::internal(msg)
             })?;
-
         let query_stage_event_sender = self
             .server
             .query_stage_event_loop
@@ -122,7 +213,6 @@ impl FlightSqlService for FlightSqlServiceImpl {
                     e
                 ))
             })?;
-
         query_stage_event_sender
             .post_event(QueryStageSchedulerEvent::JobQueued {
                 job_id: job_id.clone(),
@@ -137,6 +227,24 @@ impl FlightSqlService for FlightSqlServiceImpl {
                 error!("{}", msg);
                 Status::internal(msg)
             })?;
+        Ok(job_id)
+    }
+}
+
+#[tonic::async_trait]
+impl FlightSqlService for FlightSqlServiceImpl {
+    type FlightService = FlightSqlServiceImpl;
+    // get_flight_info
+    async fn get_flight_info_statement(
+        &self,
+        query: CommandStatementQuery,
+        _request: FlightDescriptor,
+    ) -> Result<Response<FlightInfo>, Status> {
+        debug!("Got query:\n{}", query.query);
+
+        let ctx = self.create_ctx().await?;
+        let plan = Self::prepare_statement(query, &ctx).await?;
+        let job_id = self.enqueue_job(ctx, &plan).await?;
 
         // let handle = Uuid::new_v4();
         // let mut statements = self.statements.try_lock()
@@ -148,85 +256,14 @@ impl FlightSqlService for FlightSqlServiceImpl {
         let mut num_bytes = 0;
         let fieps = loop {
             sleep(Duration::from_millis(100)).await;
-            let status = self
-                .server
-                .state
-                .task_manager
-                .get_job_status(&job_id)
-                .await
-                .map_err(|e| {
-                    let msg = format!("Error getting status for job {}: {:?}", job_id, e);
-                    error!("{}", msg);
-                    Status::internal(msg)
-                })?;
-            let status: JobStatus = match status {
-                Some(status) => status,
-                None => {
-                    let msg = format!("Error getting status for job {}!", job_id);
-                    error!("{}", msg);
-                    Err(Status::internal(msg))?
-                }
+            let completed = if let Some(comp) = self.check_job(&job_id).await? {
+                comp
+            } else {
+                continue;
             };
-            let status: job_status::Status = match status.status {
-                Some(status) => status,
-                None => {
-                    let msg = format!("Error getting status for job {}!", job_id);
-                    error!("{}", msg);
-                    Err(Status::internal(msg))?
-                }
-            };
-            let completed = match status {
-                job_status::Status::Queued(_) => continue,
-                job_status::Status::Running(_) => continue,
-                job_status::Status::Failed(e) => Err(Status::internal(format!(
-                    "Error building plan: {}",
-                    e.error
-                )))?,
-                job_status::Status::Completed(comp) => comp,
-            };
-            let mut fieps: Vec<_> = vec![];
-            for loc in completed.partition_location.iter() {
-                let fetch = if let Some(ref id) = loc.partition_id {
-                    let fetch = protobuf::FetchPartition {
-                        job_id: id.job_id.clone(),
-                        stage_id: id.stage_id,
-                        partition_id: id.partition_id,
-                        path: loc.path.clone(),
-                    };
-                    protobuf::Action {
-                        action_type: Some(protobuf::action::ActionType::FetchPartition(
-                            fetch,
-                        )),
-                        settings: vec![],
-                    }
-                } else {
-                    Err(Status::internal("Error getting partition ID".to_string()))?
-                };
-                let authority = if let Some(ref md) = loc.executor_meta {
-                    format!("{}:{}", md.host, md.port)
-                } else {
-                    Err(Status::internal(
-                        "Invalid partition location, missing executor metadata"
-                            .to_string(),
-                    ))?
-                };
-                if let Some(ref stats) = loc.partition_stats {
-                    num_rows += stats.num_rows;
-                    num_bytes += stats.num_bytes;
-                } else {
-                    Err(Status::internal("Error getting stats".to_string()))?
-                }
-                let loc = Location {
-                    uri: format!("grpc+tcp://{}", authority),
-                };
-                let buf = fetch.encode_to_vec();
-                let ticket = Ticket { ticket: buf };
-                let fiep = FlightEndpoint {
-                    ticket: Some(ticket),
-                    location: vec![loc],
-                };
-                fieps.push(fiep);
-            }
+            let fieps = self
+                .job_to_fetch_part(completed, &mut num_rows, &mut num_bytes)
+                .await?;
             break fieps;
         };
 
@@ -234,7 +271,6 @@ impl FlightSqlService for FlightSqlServiceImpl {
         let arrow_schema: Schema = (&**plan.schema()).into();
         let options = IpcWriteOptions::default();
         let pair = SchemaAsIpc::new(&arrow_schema, &options);
-
         let data_gen = IpcDataGenerator::default();
         let encoded_data = data_gen.schema_to_bytes(pair.0, pair.1);
         let mut schema_bytes = vec![];
@@ -258,6 +294,7 @@ impl FlightSqlService for FlightSqlServiceImpl {
         debug!("Responding to query...");
         Ok(resp)
     }
+
     async fn get_flight_info_prepared_statement(
         &self,
         _query: CommandPreparedStatementQuery,
