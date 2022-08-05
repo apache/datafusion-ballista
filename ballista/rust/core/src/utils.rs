008366a2240b15e58590800ef3edd1b9b18da2f1
Wang <mingmwang@ebay.com>
2022-08-05 11:46:13 +0800

    set tcp_nodelay to true explicitly

--- ballista/rust/core/src/utils.rs
+++ ballista/rust/core/src/utils.rs
@@ -324,6 +324,8 @@ where
     let endpoint = tonic::transport::Endpoint::new(dst)?
         .connect_timeout(Duration::from_secs(20))
         .timeout(Duration::from_secs(20))
+        // Disable Nagle's Algorithm since we don't want packets to wait
+        .tcp_nodelay(true)
         .tcp_keepalive(Option::Some(Duration::from_secs(3600)))
         .http2_keep_alive_interval(Duration::from_secs(300))
         .keep_alive_timeout(Duration::from_secs(20))
@@ -334,6 +336,8 @@ where
 pub fn create_grpc_server() -> Server {
     Server::builder()
         .timeout(Duration::from_secs(20))
+        // Disable Nagle's Algorithm since we don't want packets to wait
+        .tcp_nodelay(true)
         .tcp_keepalive(Option::Some(Duration::from_secs(3600)))
         .http2_keepalive_interval(Option::Some(Duration::from_secs(300)))
         .http2_keepalive_timeout(Option::Some(Duration::from_secs(20)))
