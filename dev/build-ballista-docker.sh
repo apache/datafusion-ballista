8c8b367d958a4c319613076bd1396c2ad7a907b2
Brent Gardner <brent.gardner@spaceandtime.io>
2022-09-20 17:15:26 -0600

    Fix PR feedback

--- dev/build-ballista-docker.sh
+++ dev/build-ballista-docker.sh
@@ -26,6 +26,6 @@ docker run -v $(pwd):/home/builder/workspace ballista-builder
 docker-compose build
 
 . ./dev/build-set-env.sh
-docker tag apache/arrow-ballista-executor "apache/arrow-ballista-executor:$BALLISTA_VERSION"
-docker tag apache/arrow-ballista-scheduler "apache/arrow-ballista-scheduler:$BALLISTA_VERSION"
-docker tag apache/arrow-ballista-benchmarks "apache/arrow-ballista-benchmarks:$BALLISTA_VERSION"
+docker tag ballista-executor "apache/arrow-ballista-executor:$BALLISTA_VERSION"
+docker tag ballista-scheduler "apache/arrow-ballista-scheduler:$BALLISTA_VERSION"
+docker tag ballista-benchmarks "apache/arrow-ballista-benchmarks:$BALLISTA_VERSION"
