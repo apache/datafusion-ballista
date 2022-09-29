fe1866d529cefb7ea82e717bf0fac46c49d21b8d
Faiaz Sanaulla <faiaz.sanaulla@coralogix.com>
2022-09-29 14:08:10 +0300

    sc-10300: remove commets

--- ballista/rust/scheduler/src/state/task_manager.rs
+++ ballista/rust/scheduler/src/state/task_manager.rs
@@ -393,27 +393,8 @@ impl<T: 'static + AsLogicalPlan, U: 'static + AsExecutionPlan> TaskManager<T, U>
                         self.fail_running_job(job_id).await?
                     }
                 }
-                // let reset = graph.reset_stages(executor_id)?;
-                // if !reset.is_empty() {
-                //     updated_graphs.insert(job_id.to_owned(), graph.clone());
-                // }
             }
         }
-
-        // let lock = self.state.lock(Keyspace::ActiveJobs, "").await?;
-        // with_lock(lock, async {
-        //     // Transactional update graphs
-        //     let txn_ops: Vec<(Keyspace, String, Vec<u8>)> = updated_graphs
-        //         .into_iter()
-        //         .map(|(job_id, graph)| {
-        //             let value = self.encode_execution_graph(graph)?;
-        //             Ok((Keyspace::ActiveJobs, job_id, value))
-        //         })
-        //         .collect::<Result<Vec<_>>>()?;
-        //     self.state.put_txn(txn_ops).await?;
-        //     Ok(())
-        // })
-        // .await
         Ok(())
     }
 
