16a925d2c646987d7e8b17279654f98b17629a31
Andy Grove <andygrove73@gmail.com>
2022-10-02 16:16:01 -0600

    code cleanup

--- ballista/rust/core/src/config.rs
+++ ballista/rust/core/src/config.rs
@@ -27,6 +27,7 @@ use crate::error::{BallistaError, Result};
 
 use datafusion::arrow::datatypes::DataType;
 
+pub const BALLISTA_APP_NAME: &str = "ballista.app.name";
 pub const BALLISTA_DEFAULT_SHUFFLE_PARTITIONS: &str = "ballista.shuffle.partitions";
 pub const BALLISTA_DEFAULT_BATCH_SIZE: &str = "ballista.batch.size";
 pub const BALLISTA_REPARTITION_JOINS: &str = "ballista.repartition.joins";
@@ -155,6 +156,9 @@ impl BallistaConfig {
     /// All available configuration options
     pub fn valid_entries() -> HashMap<String, ConfigEntry> {
         let entries = vec![
+            ConfigEntry::new(BALLISTA_APP_NAME.to_string(),
+                             "Sets the job name that will appear in the web user interface for any submitted jobs".to_string(),
+                             DataType::Utf8, None),
             ConfigEntry::new(BALLISTA_DEFAULT_SHUFFLE_PARTITIONS.to_string(),
                              "Sets the default number of partitions to create when repartitioning query stages".to_string(),
                              DataType::UInt16, Some("16".to_string())),
