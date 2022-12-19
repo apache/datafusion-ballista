4748c3f8e9b216bedc3fbab07eda260a2c74234e
jiangzhx <jiangzhx@gmail.com>
2022-12-19 15:44:09 +0800

    make example work

--- examples/src/lib.rs
+++ examples/src/lib.rs
@@ -15,30 +15,4 @@
 // specific language governing permissions and limitations
 // under the License.
 
-use ballista::prelude::{BallistaConfig, BallistaContext, Result};
-use ballista_examples::test_util;
-use datafusion::prelude::CsvReadOptions;
-
-#[tokio::main]
-async fn main() -> Result<()> {
-    let config = BallistaConfig::builder()
-        .set("ballista.shuffle.partitions", "1")
-        .build()?;
-
-    let ctx = BallistaContext::standalone(&config, 2).await?;
-
-    let testdata = test_util::examples_test_data();
-
-    // register csv file with the execution context
-    ctx.register_csv(
-        "test",
-        &format!("{testdata}/aggregate_test_100.csv"),
-        CsvReadOptions::new(),
-    )
-    .await?;
-
-    let df = ctx.sql("select count(1) from test").await?;
-
-    df.show().await?;
-    Ok(())
-}
+pub mod test_util;
