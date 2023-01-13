fe33d9e17860ca0ac29910ae71e7448b5acff4b8
Tim Van Wassenhove <tim@timvw.be>
2023-01-13 22:19:43 +0100

    feat: update script such that ballista-cli image is built as well

--- dev/docker/cli-entrypoint.sh
+++ dev/docker/cli-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -19,19 +19,4 @@
 
 set -e
 
-RELEASE_FLAG=${RELEASE_FLAG:=release}
-
-docker build -t ballista-builder --build-arg EXT_UID="$(id -u)" -f dev/docker/ballista-builder.Dockerfile .
-
-docker run -v $(pwd):/home/builder/workspace --env RELEASE_FLAG=$RELEASE_FLAG ballista-builder
-
-docker-compose build
-
-. ./dev/build-set-env.sh
-docker build -t "apache/arrow-ballista-standalone:$BALLISTA_VERSION" -f dev/docker/ballista-standalone.Dockerfile .
-
-docker tag ballista-executor "apache/arrow-ballista-executor:$BALLISTA_VERSION"
-docker tag ballista-scheduler "apache/arrow-ballista-scheduler:$BALLISTA_VERSION"
-docker tag ballista-benchmarks "apache/arrow-ballista-benchmarks:$BALLISTA_VERSION"
-
-docker build -t "apache/arrow-ballista-cli:$BALLISTA_VERSION" -f dev/docker/ballista-cli.Dockerfile .
+/root/ballista-cli "$@"
