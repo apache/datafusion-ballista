78ea066555b95c738131301ef67405a8eb9ede3a
yangjiang <yangjiang@ebay.com>
2023-04-10 14:19:20 +0800

    [feature] support shuffle read with retry when facing IO error.

--- ballista/core/src/client.rs
+++ ballista/core/src/client.rs
@@ -43,15 +43,18 @@ use crate::serde::protobuf;
 use crate::utils::create_grpc_client_connection;
 use datafusion::physical_plan::{RecordBatchStream, SendableRecordBatchStream};
 use futures::{Stream, StreamExt};
-use log::debug;
+use log::{debug, warn};
 use prost::Message;
-use tonic::Streaming;
+use tonic::{Code, Streaming};
 
 /// Client for interacting with Ballista executors.
 #[derive(Clone)]
 pub struct BallistaClient {
     flight_client: FlightServiceClient<tonic::transport::channel::Channel>,
 }
+
+const IO_RETRIES_TIMES: u8 = 3;
+const IO_RETRY_WAIT_TIME_MS: u64 = 5000;
 
 impl BallistaClient {
     /// Create a new BallistaClient to connect to the executor listening on the specified
@@ -64,8 +67,8 @@ impl BallistaClient {
                 .await
                 .map_err(|e| {
                     BallistaError::GrpcConnectionError(format!(
-                    "Error connecting to Ballista scheduler or executor at {addr}: {e:?}"
-                ))
+                        "Error connecting to Ballista scheduler or executor at {addr}: {e:?}"
+                    ))
                 })?;
         let flight_client = FlightServiceClient::new(connection);
         debug!("BallistaClient connected OK");
@@ -117,32 +120,61 @@ impl BallistaClient {
             .encode(&mut buf)
             .map_err(|e| BallistaError::GrpcActionError(format!("{e:?}")))?;
 
-        let request = tonic::Request::new(Ticket { ticket: buf.into() });
-
-        let mut stream = self
-            .flight_client
-            .do_get(request)
-            .await
-            .map_err(|e| BallistaError::GrpcActionError(format!("{e:?}")))?
-            .into_inner();
-
-        // the schema should be the first message returned, else client should error
-        match stream
-            .message()
-            .await
-            .map_err(|e| BallistaError::GrpcActionError(format!("{e:?}")))?
-        {
-            Some(flight_data) => {
-                // convert FlightData to a stream
-                let schema = Arc::new(Schema::try_from(&flight_data)?);
-
-                // all the remaining stream messages should be dictionary and record batches
-                Ok(Box::pin(FlightDataStream::new(stream, schema)))
+        for i in 0..IO_RETRIES_TIMES {
+            if i > 0 {
+                warn!("Remote shuffle read fail, retry {} times, sleep {} ms.", i, IO_RETRY_WAIT_TIME_MS);
+                tokio::time::sleep(std::time::Duration::from_millis(
+                    IO_RETRY_WAIT_TIME_MS
+                )).await;
             }
-            None => Err(BallistaError::GrpcActionError(
-                "Did not receive schema batch from flight server".to_string(),
-            )),
+
+            let request = tonic::Request::new(Ticket { ticket: buf.clone().into() });
+            let result = self
+                .flight_client
+                .do_get(request)
+                .await;
+
+            let res = match result {
+                Ok(res) => { res }
+                Err(ref err) => {
+                    // IO related error like connection timeout, reset... will warp with Code::Internal
+                    // This means IO related error will retry.
+                    if i == IO_RETRIES_TIMES - 1 || err.code() != Code::Internal {
+                        return BallistaError::GrpcActionError(format!("{:?}", result.unwrap_err())).into();
+                    }
+                    // retry request
+                    continue;
+                }
+            };
+
+            let mut stream = res.into_inner();
+            match stream
+                .message()
+                .await
+            {
+                Ok(res) => {
+                    return match res {
+                        Some(flight_data) => {
+                            // convert FlightData to a stream
+                            let schema = Arc::new(Schema::try_from(&flight_data)?);
+
+                            // all the remaining stream messages should be dictionary and record batches
+                            Ok(Box::pin(FlightDataStream::new(stream, schema)))
+                        }
+                        None => Err(BallistaError::GrpcActionError(
+                            "Did not receive schema batch from flight server".to_string(),
+                        )),
+                    };
+                }
+                Err(e) => {
+                    if i == IO_RETRIES_TIMES - 1 {
+                        return BallistaError::GrpcActionError(format!("{:?}", e.to_string())).into();
+                    }
+                    continue;
+                }
+            }
         }
+        unreachable!("Did not receive schema batch from flight server");
     }
 }
 
@@ -179,7 +211,7 @@ impl Stream for FlightDataStream {
                             self.schema.clone(),
                             &self.dictionaries_by_id,
                         )
-                        .map_err(DataFusionError::ArrowError)
+                            .map_err(DataFusionError::ArrowError)
                     });
                 Some(converted_chunk)
             }
