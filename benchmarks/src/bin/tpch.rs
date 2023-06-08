77a68a4637bdc7d9a9c8341137942755ea6a77cd
Daniël Heres <daniel.heres@coralogix.com>
2023-06-08 21:42:23 +0200

    Add physical_round_trip test

--- benchmarks/src/bin/tpch.rs
+++ benchmarks/src/bin/tpch.rs
@@ -1730,7 +1730,7 @@ mod ballista_round_trip {
     test_round_trip_physical!(physical_round_trip_q12, 12);
     test_round_trip_physical!(physical_round_trip_q13, 13);
     test_round_trip_physical!(physical_round_trip_q14, 14);
-    // test_round_trip_physical!(physical_round_trip_q15, 15); // https://github.com/apache/arrow-ballista/issues/330
+    test_round_trip_physical!(physical_round_trip_q15, 15);
     test_round_trip_physical!(physical_round_trip_q16, 16);
     test_round_trip_physical!(physical_round_trip_q17, 17);
     test_round_trip_physical!(physical_round_trip_q18, 18);
