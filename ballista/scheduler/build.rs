8e1b512f19a19893d638da6a8e38ddd9310854ae
Marko Milenković <milenkovicm@users.noreply.github.com>
2025-01-21 21:15:19 +0000

    fix: scheduler compiles keda even if it's disabled
    which will require `protoc` installed which affects
    user experience.
    
    with this patch `protoc` will be required only if
    `keda-scaler` feature is enabled.

--- ballista/scheduler/build.rs
+++ ballista/scheduler/build.rs
@@ -18,12 +18,18 @@
 fn main() -> Result<(), String> {
     #[cfg(feature = "build-binary")]
     println!("cargo:rerun-if-changed=scheduler_config_spec.toml");
+
     #[cfg(feature = "build-binary")]
     configure_me_codegen::build_script_auto()
         .map_err(|e| format!("configure_me code generation failed: {e}"))?;
 
+    #[cfg(feature = "keda-scaler")]
     println!("cargo:rerun-if-changed=proto/keda.proto");
+
+    #[cfg(feature = "keda-scaler")]
     tonic_build::configure()
         .compile_protos(&["proto/keda.proto"], &["proto"])
-        .map_err(|e| format!("protobuf compilation failed: {e}"))
+        .map_err(|e| format!("protobuf compilation failed: {e}"))?;
+
+    Ok(())
 }
