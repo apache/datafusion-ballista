0ab0ec8bae4be8ecc79ee866de1684e7eab4a81c
Marko Milenković <milenkovicm@users.noreply.github.com>
2025-04-04 20:29:08 +0100

    rename create_session to create_or_update session

--- ballista/scheduler/src/scheduler_server/grpc.rs
+++ ballista/scheduler/src/scheduler_server/grpc.rs
@@ -280,7 +280,7 @@ impl<T: 'static + AsLogicalPlan, U: 'static + AsExecutionPlan> SchedulerGrpc
         let ctx = self
             .state
             .session_manager
-            .create_session("!!! CHANGE ME !!!", &session_config)
+            .create_or_update_session("!!! CHANGE ME !!!", &session_config)
             .await
             .map_err(|e| {
                 Status::internal(format!("Failed to create SessionContext: {e:?}"))
@@ -343,7 +343,7 @@ impl<T: 'static + AsLogicalPlan, U: 'static + AsExecutionPlan> SchedulerGrpc
                     let ctx = self
                         .state
                         .session_manager
-                        .create_session(&session_id, &session_config)
+                        .create_or_update_session(&session_id, &session_config)
                         .await
                         .map_err(|e| {
                             Status::internal(format!(
