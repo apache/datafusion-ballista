4ed215c23faf2227352bae568740f4faffa5beb1
Marko Milenković <milenkovicm@users.noreply.github.com>
2025-04-19 08:23:09 +0100

    chore: add read/write roundtrip tests

--- ballista/client/tests/common/mod.rs
+++ ballista/client/tests/common/mod.rs
@@ -239,9 +239,7 @@ fn init() {
     // Enable RUST_LOG logging configuration for test
     let _ = env_logger::builder()
         .filter_level(log::LevelFilter::Info)
-        .parse_filters(
-            "ballista=debug,ballista_scheduler-rs=debug,ballista_executor=debug",
-        )
+        .parse_filters("ballista=debug,ballista_scheduler=debug,ballista_executor=debug")
         //.parse_filters("ballista=debug,ballista_scheduler-rs=debug,ballista_executor=debug,datafusion=debug")
         .is_test(true)
         .try_init();
