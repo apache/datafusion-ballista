9b91d9010bbdafb8e759b3ace083b1899469649e
Marko Milenković <milenkovicm@users.noreply.github.com>
2025-05-18 10:06:38 +0100

    feat: expose submit and cancel job methods as public in scheduler (#1260)
    * expose schedule and cancel job methods as public
    
    * address code review comment

--- ballista/scheduler/src/scheduler_server/query_stage_scheduler.rs
+++ ballista/scheduler/src/scheduler_server/query_stage_scheduler.rs
@@ -376,9 +376,7 @@ mod tests {
         )
         .await?;
 
-        let job_id = "job-1";
-
-        test.submit(job_id, "", &plan).await?;
+        let job_id = test.submit("", &plan).await?;
 
         test.tick().await?;
 
@@ -398,7 +396,7 @@ mod tests {
             expected, running_jobs
         );
 
-        test.cancel(job_id).await?;
+        test.cancel(&job_id).await?;
 
         let expected = 0usize;
         let success = await_condition(Duration::from_millis(10), 20, || {
