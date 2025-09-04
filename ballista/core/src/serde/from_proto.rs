059988aac3eb1419fb621c999679325e83ab2a81
Daniel Tu <danieltu.life@gmail.com>
2025-09-03 22:57:02 -0700

    support distributed plan in explan

---
+++ ballista/core/src/serde/from_proto.rs
@@ -0,0 +1,43 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use crate::execution_plans::BallistaPlanType;
+use crate::serde::protobuf;
+use datafusion_proto::protobuf as df_protobuf;
+
+/// Convert protobuf BallistaPlanType to Ballista BallistaPlanType
+impl From<&protobuf::BallistaPlanType> for BallistaPlanType {
+    fn from(proto_type: &protobuf::BallistaPlanType) -> Self {
+        use protobuf::ballista_plan_type::PlanType;
+        
+        match &proto_type.plan_type {
+            Some(PlanType::DatafusionPlanType(df_type)) => {
+                // Create a dummy protobuf StringifiedPlan to leverage DataFusion's conversion
+                let dummy_proto_plan = df_protobuf::StringifiedPlan {
+                    plan_type: Some(df_type.clone()),
+                    plan: "".to_string(),
+                };
+                let df_plan: datafusion::logical_expr::StringifiedPlan = (&dummy_proto_plan).into();
+                BallistaPlanType::DataFusionPlanType(df_plan.plan_type)
+            },
+            Some(PlanType::DistributedPlan(_)) => {
+                BallistaPlanType::DistributedPlan
+            },
+            None => panic!("Missing plan_type variant in BallistaPlanType"),
+        }
+    }
+}
