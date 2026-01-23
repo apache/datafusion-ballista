c5a4a1bac2830f97a36669fe9ceacb845000bf40
Sebastian Eckweiler <seb.eckweiler@gmail.com>
2026-01-23 16:45:18 +0100

    adjust clap args for default flight proxy

--- ballista/scheduler/src/config.rs
+++ ballista/scheduler/src/config.rs
@@ -41,6 +41,8 @@ pub struct Config {
     /// Route for proxying flight results via scheduler (IP:PORT format).
     #[arg(
         long,
+        num_args = 0..=1,
+        default_missing_value = "",
         help = "Route for proxying flight results via scheduler. Use 'HOST:PORT' to let clients fetch results from the specified address. If empty a flight proxy will be started on the scheduler host and port."
     )]
     pub advertise_flight_sql_endpoint: Option<String>,
