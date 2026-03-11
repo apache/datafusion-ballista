8fd5eb47e1e1ff5be5ea4abe8c075c3b802884fd
Marko Milenković <milenkovicm@users.noreply.github.com>
2026-03-11 10:25:42 +0000

    fix compile build

--- ballista/scheduler/src/api/handlers.rs
+++ ballista/scheduler/src/api/handlers.rs
@@ -201,7 +201,7 @@ pub async fn get_job<
         .get_job_execution_graph(&job_id)
         .await
         .map_err(|err| {
-            tracing::error!("Error occurred while getting the execution graph for job '{job_is}': {err:?}");
+            tracing::error!("Error occurred while getting the execution graph for job '{job_id}': {err:?}");
             StatusCode::INTERNAL_SERVER_ERROR
         })?
         .ok_or(StatusCode::NOT_FOUND)?;
