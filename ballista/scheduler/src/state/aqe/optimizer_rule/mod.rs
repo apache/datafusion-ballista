f4020b01a2a0586589e41fa59e70c402ef09dc2a
sandudb <alexandrdomenti@gmail.com>
2026-04-27 13:34:30 +0300

    Removed datafusion_patch entirely due to datafusion ver upgrade

--- ballista/scheduler/src/state/aqe/optimizer_rule/mod.rs
+++ ballista/scheduler/src/state/aqe/optimizer_rule/mod.rs
@@ -15,12 +15,10 @@
 // specific language governing permissions and limitations
 // under the License.
 
-pub mod datafusion_patch;
 pub mod distributed_exchange;
 pub mod eliminate_empty;
 pub mod propagate_empty;
 
-pub use datafusion_patch::*;
 pub use distributed_exchange::*;
 pub use eliminate_empty::*;
 pub use propagate_empty::*;
