e3523673d61d25041d1b5cd814dd167cf6b12687
Daniel Tu <danieltu.life@gmail.com>
2026-04-28 03:49:56 -0700

    feat: Support `EXPLAIN ANALYZE` in Ballista (#1567)
    * prototype
    
    * fix ci errors
    
    * naming

--- ballista/core/src/serde/generated/ballista.rs
+++ ballista/core/src/serde/generated/ballista.rs
@@ -1019,6 +1019,36 @@ pub struct GetJobStatusParams {
     #[prost(string, tag = "1")]
     pub job_id: ::prost::alloc::string::String,
 }
+#[derive(Clone, PartialEq, Eq, Hash, ::prost::Message)]
+pub struct GetJobMetricsParams {
+    #[prost(string, tag = "1")]
+    pub job_id: ::prost::alloc::string::String,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct JobStageMetrics {
+    #[prost(uint32, tag = "1")]
+    pub stage_id: u32,
+    #[prost(uint32, tag = "2")]
+    pub partitions: u32,
+    #[prost(message, repeated, tag = "3")]
+    pub operators: ::prost::alloc::vec::Vec<OperatorWithMetrics>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct OperatorWithMetrics {
+    #[prost(uint32, tag = "1")]
+    pub depth: u32,
+    #[prost(string, tag = "2")]
+    pub operator_type: ::prost::alloc::string::String,
+    #[prost(string, tag = "3")]
+    pub operator_desc: ::prost::alloc::string::String,
+    #[prost(message, repeated, tag = "4")]
+    pub metrics: ::prost::alloc::vec::Vec<OperatorMetric>,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct GetJobMetricsResult {
+    #[prost(message, repeated, tag = "1")]
+    pub stages: ::prost::alloc::vec::Vec<JobStageMetrics>,
+}
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct SuccessfulJob {
     #[prost(message, repeated, tag = "1")]
@@ -1512,6 +1542,32 @@ pub mod scheduler_grpc_client {
                 );
             self.inner.unary(req, path, codec).await
         }
+        pub async fn get_job_metrics(
+            &mut self,
+            request: impl tonic::IntoRequest<super::GetJobMetricsParams>,
+        ) -> std::result::Result<
+            tonic::Response<super::GetJobMetricsResult>,
+            tonic::Status,
+        > {
+            self.inner
+                .ready()
+                .await
+                .map_err(|e| {
+                    tonic::Status::unknown(
+                        format!("Service was not ready: {}", e.into()),
+                    )
+                })?;
+            let codec = tonic_prost::ProstCodec::default();
+            let path = http::uri::PathAndQuery::from_static(
+                "/ballista.protobuf.SchedulerGrpc/GetJobMetrics",
+            );
+            let mut req = request.into_request();
+            req.extensions_mut()
+                .insert(
+                    GrpcMethod::new("ballista.protobuf.SchedulerGrpc", "GetJobMetrics"),
+                );
+            self.inner.unary(req, path, codec).await
+        }
         /// Used by Executor to tell Scheduler it is stopped.
         pub async fn executor_stopped(
             &mut self,
@@ -1668,6 +1724,13 @@ pub mod scheduler_grpc_server {
             request: tonic::Request<super::GetJobStatusParams>,
         ) -> std::result::Result<
             tonic::Response<super::GetJobStatusResult>,
+            tonic::Status,
+        >;
+        async fn get_job_metrics(
+            &self,
+            request: tonic::Request<super::GetJobMetricsParams>,
+        ) -> std::result::Result<
+            tonic::Response<super::GetJobMetricsResult>,
             tonic::Status,
         >;
         /// Used by Executor to tell Scheduler it is stopped.
@@ -2165,6 +2228,51 @@ pub mod scheduler_grpc_server {
                     let inner = self.inner.clone();
                     let fut = async move {
                         let method = GetJobStatusSvc(inner);
+                        let codec = tonic_prost::ProstCodec::default();
+                        let mut grpc = tonic::server::Grpc::new(codec)
+                            .apply_compression_config(
+                                accept_compression_encodings,
+                                send_compression_encodings,
+                            )
+                            .apply_max_message_size_config(
+                                max_decoding_message_size,
+                                max_encoding_message_size,
+                            );
+                        let res = grpc.unary(method, req).await;
+                        Ok(res)
+                    };
+                    Box::pin(fut)
+                }
+                "/ballista.protobuf.SchedulerGrpc/GetJobMetrics" => {
+                    #[allow(non_camel_case_types)]
+                    struct GetJobMetricsSvc<T: SchedulerGrpc>(pub Arc<T>);
+                    impl<
+                        T: SchedulerGrpc,
+                    > tonic::server::UnaryService<super::GetJobMetricsParams>
+                    for GetJobMetricsSvc<T> {
+                        type Response = super::GetJobMetricsResult;
+                        type Future = BoxFuture<
+                            tonic::Response<Self::Response>,
+                            tonic::Status,
+                        >;
+                        fn call(
+                            &mut self,
+                            request: tonic::Request<super::GetJobMetricsParams>,
+                        ) -> Self::Future {
+                            let inner = Arc::clone(&self.0);
+                            let fut = async move {
+                                <T as SchedulerGrpc>::get_job_metrics(&inner, request).await
+                            };
+                            Box::pin(fut)
+                        }
+                    }
+                    let accept_compression_encodings = self.accept_compression_encodings;
+                    let send_compression_encodings = self.send_compression_encodings;
+                    let max_decoding_message_size = self.max_decoding_message_size;
+                    let max_encoding_message_size = self.max_encoding_message_size;
+                    let inner = self.inner.clone();
+                    let fut = async move {
+                        let method = GetJobMetricsSvc(inner);
                         let codec = tonic_prost::ProstCodec::default();
                         let mut grpc = tonic::server::Grpc::new(codec)
                             .apply_compression_config(
