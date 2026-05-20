274d639bc083dd39e4e847aba29687823553ff64
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2026-05-20 17:08:04 +0300

    Allow CORS only from localhost

--- ballista/scheduler/src/api/routes.rs
+++ ballista/scheduler/src/api/routes.rs
@@ -16,7 +16,7 @@ use axum::{Router, routing::get};
 use datafusion_proto::logical_plan::AsLogicalPlan;
 use datafusion_proto::physical_plan::AsExecutionPlan;
 use std::sync::Arc;
-use tower_http::cors::CorsLayer;
+use tower_http::cors::{AllowOrigin, CorsLayer};
 
 /// All routes configured for rest-api.
 pub fn get_routes<
@@ -58,7 +58,9 @@ pub fn get_routes<
         get(handlers::get_job_svg_graph::<T, U>),
     );
 
-    router
-        .with_state(scheduler_server)
-        .layer(CorsLayer::permissive())
+    let cors = CorsLayer::new()
+        .allow_origin(AllowOrigin::exact("http://localhost:8080".parse().unwrap()))
+        .allow_methods([http::Method::GET, http::Method::PATCH]);
+
+    router.with_state(scheduler_server).layer(cors)
 }
