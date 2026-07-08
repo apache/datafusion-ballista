448a925eebf08643e0f7801031ce92f0de9d784f
Andy Grove <agrove@apache.org>
2026-07-08 12:00:50 -0600

    test: add dataless stats-injecting table provider for plan stability

---
+++ ballista/scheduler/tests/tpch_plan_stability/main.rs
@@ -0,0 +1,45 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+mod stats_table;
+
+use std::sync::Arc;
+
+use datafusion::arrow::datatypes::{DataType, Field, Schema};
+use datafusion::common::stats::Precision;
+use datafusion::prelude::SessionContext;
+
+use stats_table::TpchStatsTable;
+
+#[tokio::test]
+async fn stats_table_reports_injected_rows() {
+    let schema = Arc::new(Schema::new(vec![Field::new("a", DataType::Int64, false)]));
+    let ctx = SessionContext::new();
+    ctx.register_table("t", Arc::new(TpchStatsTable::new(Arc::clone(&schema), 12_345)))
+        .unwrap();
+
+    let plan = ctx
+        .sql("SELECT a FROM t")
+        .await
+        .unwrap()
+        .create_physical_plan()
+        .await
+        .unwrap();
+
+    let stats = plan.partition_statistics(None).unwrap();
+    assert_eq!(stats.num_rows, Precision::Inexact(12_345));
+}
